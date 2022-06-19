29bb99a8541fb7c2d3f7a30e5639159f425d4a29
satip1 <satip1@yandex.ru>
2022-06-19 20:26:25 +0300

    ПР13: коррекция

--- controllers/users.js
+++ controllers/users.js
@@ -35,24 +35,17 @@ module.exports.patchUserData = (req, res) => {
 
   // обновляем данные
   User.findByIdAndUpdate(owner, { name: nameUser, about: aboutUser }, { new: true })
-    .then((user) => res.send({ message: `Данные пользователя обновили` }))
+    .then((user) => res.send({ message: 'Данные пользователя обновили' }))
     .catch((err) => res.status(500).send({ message: `Запрос на обновление данных пользователя. Ошибка: ${err}` }));
 };
 
 // обновляем аватар пользователя
 module.exports.patchUserAvatar = (req, res) => {
   const owner = req.user._id; // заглушка
-  const { avatarUser } = req.body;
-  // проверяем данные о пользователе
-  if (!avatarUser) return res.status(400).send({ message: 'Ошибка: пустое имя пользователя' });
-  User.findById(owner)
-    .then((user) => {
-      if (!user) return res.status(404).send({ message: 'Ошибка: пользователь с таким id не найден' });
-    })
-    .catch((err) => res.status(500).send({ message: `Запрос на создание пользователя. Ошибка: ${err}` }));
+  const { avatar } = req.body;
 
   // обновляем аватар
-  User.findByIdAndUpdate(owner, { avatar: avatarUser }, { new: true })
+  User.findByIdAndUpdate(owner, { avatar: avatar }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => res.status(500).send({ message: `Запрос на обновление аватара пользователя. Ошибка: ${err}` }));
 };
