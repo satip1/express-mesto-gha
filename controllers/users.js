253febe7954434ea0e18c1751eddb0ce31d7d9cf
satip1 <satip1@yandex.ru>
2022-06-21 06:22:35 +0300

    ПР13: коррекция

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,7 @@ module.exports.getIdUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (user) res.status(OK).send({ user })
-      else res.status(ERROR_DATA).send({ message: 'Пользователь с данным id не существует' });
+      else res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь с данным id не существует' });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
