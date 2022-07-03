bc6ea292fe57777d726ed420b18f0a7acb2d593d
satip1 <satip1@yandex.ru>
2022-07-03 09:56:42 +0300

    ПР14: первая итерация

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,8 @@
 
 const jwt = require('jsonwebtoken');
 
-const { ErrorLogin } = require('../errors/ErrorLogin');
+// const { ErrorLogin } = require('../errors/ErrorLogin');
+const { ErrorOtherError } = require('../errors/ErrorOtherError');
 const { SECRET_CODE } = require('../constants/constants');
 
 module.exports.auth = (req, res, next) => {
@@ -10,7 +11,7 @@ module.exports.auth = (req, res, next) => {
 
   // если авторизации нет или не содержит Bearer
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new ErrorLogin('Необходима авторизация'));
+    return next(new ErrorOtherError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -19,7 +20,7 @@ module.exports.auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, SECRET_CODE);
   } catch (err) {
-    return next(new ErrorLogin('Необходима авторизация'));
+    return next(new ErrorOtherError('Необходима авторизация'));
   }
 
   req.user = payload;
