31a79040da9e4eae49fe8ff8a2aa2e95942651d0
satip1 <satip1@yandex.ru>
2022-07-05 12:09:09 +0300

    ПР14: первая итерация

--- routes/cards.js
+++ routes/cards.js
@@ -13,8 +13,8 @@ router.get('/cards', getAllCards);
 
 router.post('/cards', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string().min(2).pattern(REG_LINK),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(REG_LINK),
   }),
 }), creatCard);
 
